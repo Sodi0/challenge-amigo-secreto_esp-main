bd8c2940ff1bf290f68222b88659fa1abc968ebc
Nelly Aviles <avilesnelly001@gmail.com>
2025-01-19 02:48:09 -0300

    Implementar función para actualizar la lista de amigos en HTML

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 let amigos = [];
 
-function agregarAmigo(){
-    let nombreAmigo = document.getElementById('amigo').value;
-    if (nombreAmigo == [""]) {
-        alert("Por favor, inserte un nombre");
-    } else {
-        amigos.push(nombreAmigo);
-        document.querySelector('#amigo').value = '';
-    }
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById("amigo").value;
+  if (nombreAmigo == "") {
+    alert("Por favor, inserte un nombre");
+  } else {
+    amigos.push(nombreAmigo);
+    document.querySelector("#amigo").value = "";
+    mostrarAmigo();
+  }
+}
+
+function mostrarAmigo() {
+  let listaAmigos = document.querySelector("#listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let index = 0; index < amigos.length; index++) {
+    const element = amigos[index];
+
+    let listaHTML = document.createElement("li");
+    listaHTML.textContent = element;
+    listaAmigos.appendChild(listaHTML);
+  }
 }
